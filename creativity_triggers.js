ab4d7b9ebe484ebfdcbea1a5912cfa4e9f652f49
Activeone/mateo.alvarez <mateo.alvarez@activeone.co>
2022-09-11 14:50:42 -0500

    Moved creativity triggers

---
+++ creativity_triggers.js
@@ -0,0 +1,79 @@
+const creativityTriggers = {
+    '01': {
+        name: 'Accessibility',
+        expectations: [{
+                wordSet: ['un', 'es', 'ejemplo', 'esto'],
+                answer: 'Esto es un ejemplo',
+                pending: true
+            },
+            {
+                wordSet: ['dos', 'numero', 'ejemplo'],
+                answer: 'ejemplo numero dos',
+                pending: true
+            }
+        ]
+    },
+    '02': {
+        name: 'Eco-friendly'
+    },
+    '03': {
+        name: 'Portable'
+    },
+    '04': {
+        name: 'Refined'
+    },
+    '05': {
+        name: 'Powerful'
+    },
+    '06': {
+        name: 'Involvement'
+    },
+    '07': {
+        name: 'Ingenious'
+    },
+    '08': {
+        name: 'Attractive'
+    },
+    '09': {
+        name: 'Uncommon'
+    },
+    '10': {
+        name: 'Cohesion'
+    },
+    '11': {
+        name: 'Amusement'
+    },
+    '12': {
+        name: 'Surprising'
+    },
+    '13': {
+        name: 'Smallness'
+    },
+    '14': {
+        name: 'Comfort'
+    },
+    '15': {
+        name: 'Novelty'
+    },
+    '16': {
+        name: 'Technology'
+    },
+    '17': {
+        name: 'Feeling'
+    },
+    '18': {
+        name: 'Independence'
+    },
+    '19': {
+        name: 'Conception'
+    },
+    '20': {
+        name: 'Humerous'
+    },
+    '21': {
+        name: 'Price'
+    },
+    '22': {
+        name: 'Up-to-date'
+    },
+};
